b6352d36d1a5b56c1c392c223bf8c41fa35bf1d7
Sebastian Place <sebplace94@gmail.com>
2016-09-11 19:09:24 +0100

    add exercises route

---
+++ app/routes/exercises.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+let exercises = [
+  {
+    id:1,
+    name:"Push Up",
+    description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id:2,
+    name:"Wide-grip Pull Up",
+    description:"Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }
+];
+
+export default Ember.Route.extend({
+  model() {
+    return exercises;
+  }
+});
